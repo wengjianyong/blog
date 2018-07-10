8364fee3e19cb1c0a2ae7d60d1f590bb4ee61d9c
wengjianyong <82310763@qq.com>
2018-07-10 23:18:56 +0800

    #添加setting表

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'phone' => $data['phone'],
             'password' => Hash::make($data['password']),
         ]);
     }
